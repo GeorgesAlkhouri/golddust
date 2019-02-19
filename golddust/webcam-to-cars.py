79bb8df962479960ce439f4fc550a29b7667aaa2
Georges Alkhouri <alkhouri@informatik.uni-leipzig.de>
2019-02-19 10:42:50 +0100

    [CHANGE] req file and webcam.py file

--- golddust/webcam-to-cars.py
+++ golddust/webcam-to-cars.py
@@ -13,16 +13,17 @@ from pathlib import Path
 from datetime import datetime
 
 
-
 # Configuration that will be used by the Mask-RCNN library
 class MaskRCNNConfig(mrcnn.config.Config):
     NAME = 'coco_pretrained_model_config'
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 2
     GPU_COUNT = 1
     NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
     DETECTION_MIN_CONFIDENCE = 0.3
 
 # Filter a list of Mask R-CNN detection results to get only the detected cars busses and trucks
+
+
 def get_car_boxes(boxes, class_ids):
     car_boxes = []
 
@@ -33,8 +34,9 @@ def get_car_boxes(boxes, class_ids):
 
     return np.array(car_boxes)
 
+
 # Root directory of the project
-ROOT_DIR = Path('./golddust/webcam')
+ROOT_DIR = Path('./data')
 
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, 'logs')
@@ -47,24 +49,36 @@ if not os.path.exists(COCO_MODEL_PATH):
     mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
 
 # Path to images, replace with correct path
-IMAGE_PATH = '/Users/m/Nextcloud/luftdaten/zwei/image-test/'
+IMAGE_PATH = '/Users/georges/Downloads/images'
 
-images = {datetime.strptime(os.path.basename(file)[:-4], '%Y-%m-%d_%H:%M'): cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB) for file in glob.glob(IMAGE_PATH + '/*.jpg')}
+images = {datetime.strptime(os.path.basename(file)[:-4], '%Y-%m-%d_%H:%M'): cv2.cvtColor(
+    cv2.imread(file), cv2.COLOR_BGR2RGB) for file in glob.glob(IMAGE_PATH + '/*.jpg')}
 
 # Create a Mask-RCNN model in inference mode
-model = MaskRCNN(mode='inference', model_dir=MODEL_DIR, config=MaskRCNNConfig())
+model = MaskRCNN(mode='inference', model_dir=MODEL_DIR,
+                 config=MaskRCNNConfig())
 
 # Load pre-trained model
 model.load_weights(COCO_MODEL_PATH, by_name=True)
 
+print(model.keras_model.summary())
+
 cars = {}
 
-for date, image in images.items():
-    results = model.detect([image], verbose=0)
-    r = results[0]
-    car_boxes = get_car_boxes(r['rois'], r['class_ids'])
-    cars[date] = len(car_boxes)
 
+print(len(images.values()))
+
+results = model.detect(list(images.values())[:2])
+
+print(len(results))
+
+# for date, image in images.values():
+#     results = model.detect([image], verbose=0)
+#     print(results)
+#     break
+#     #r = results[0]
+#     #car_boxes = get_car_boxes(r['rois'], r['class_ids'])
+#     #cars[date] = len(car_boxes)
 
 
 # write cars to df
